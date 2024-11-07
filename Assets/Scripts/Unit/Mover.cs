d4e58094b512e2cd44cafb0685bc8c1096aea73b
Astor-S <154661724+Astor-S@users.noreply.github.com>
2024-11-07 14:46:54 +0300

    Update Mover.cs

--- Assets/Scripts/Unit/Mover.cs
+++ Assets/Scripts/Unit/Mover.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
@@ -5,18 +6,26 @@ public class Mover : MonoBehaviour
     [SerializeField] private float _speed = 2f;
 
     private Transform _targetTransform;
-
-    private void Update()
-    {
-        if (_targetTransform != null)
-        {
-            transform.position = Vector3.MoveTowards
-                (transform.position, _targetTransform.position, _speed * Time.deltaTime);
-        }
-    }
+    private Coroutine _moveCoroutine;
 
     public void MoveTo(Transform target)
     {
         _targetTransform = target;
+
+        if (_moveCoroutine != null)
+            StopCoroutine(_moveCoroutine);
+        
+        _moveCoroutine = StartCoroutine(MoveTowardsTarget());
+    }
+
+    private IEnumerator MoveTowardsTarget()
+    {
+        while (_targetTransform != null)
+        {
+            transform.position = Vector3.MoveTowards(
+                transform.position, _targetTransform.position, _speed * Time.deltaTime);
+
+            yield return null;
+        }
     }
 }
